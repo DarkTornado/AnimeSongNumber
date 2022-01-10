209f2ae29a749116b26887fd783e7a3e76fb19d2
Dark Tornado <dt3141592@naver.com>
2022-01-11 05:42:14 +0900

    블록 깊이 줄이기

--- script.js
+++ script.js
@@ -18,31 +18,31 @@ function showData(data) {
     var src = "", count = 0;
 
     for (var n = 0; n < data.length; n++) {
-        if (data[n].includes(input)) {
-            var datum = data[n].split("::");
-            var num = "";
-            if (datum[0] != "null") {
-                num = "금영: " + datum[0];
-            }
-            if (datum[1] != "null") {
-                if (num != "") num += " / ";
-                num += "태진: " + datum[1];
-            }
-            src += "<tr align=center bgcolor=#E0F7FA>";
-            src += "<td colspan=2><b>" + datum[2] + "</b></td>";
-            src += "</tr>";
-            src += "<tr align=center>";
-            src += "<td width=20%>가수</td><td>" + datum[3] + "</td>";
-            src += "</tr>";
-            src += "<tr align=center>";
-            src += "<td>번호</td><td>" + num + "</td>";
-            src += "</tr>";
-            src += "<tr align=center>";
-            src += "<td>비고</td><td>" + datum[4] + "</td>";
-            src += "</tr>";
-            count++;
+        if (!data[n].includes(input)) continue;
+        var datum = data[n].split("::");
+        var num = "";
+        if (datum[0] != "null") {
+            num = "금영: " + datum[0];
         }
+        if (datum[1] != "null") {
+            if (num != "") num += " / ";
+            num += "태진: " + datum[1];
+        }
+        src += "<tr align=center bgcolor=#E0F7FA>";
+        src += "<td colspan=2><b>" + datum[2] + "</b></td>";
+        src += "</tr>";
+        src += "<tr align=center>";
+        src += "<td width=20%>가수</td><td>" + datum[3] + "</td>";
+        src += "</tr>";
+        src += "<tr align=center>";
+        src += "<td>번호</td><td>" + num + "</td>";
+        src += "</tr>";
+        src += "<tr align=center>";
+        src += "<td>비고</td><td>" + datum[4] + "</td>";
+        src += "</tr>";
+        count++;
     }
+
     if (count == 0) {
         alert("검색 결과가 없습니다.");
     } else {
